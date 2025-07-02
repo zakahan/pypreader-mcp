a7a69b4fed20d5326fcfc156466d4a28e98469d2
hanzhi.421 <hanzhi.421@bytedance.com>
2025-07-02 22:02:33 +0800

    fix: test uvx by fastmcp

--- examples/fastmcp_client.py
+++ examples/fastmcp_client.py
@@ -1,5 +1,4 @@
-# uvx --from git+https://github.com/zakahan/pypreader-mcp.git pypreader-mcp
-
+import sys
 import asyncio
 from fastmcp import Client
 from fastmcp.client.transports import UvxStdioTransport
@@ -8,15 +7,26 @@ from fastmcp.client.transports import UvxStdioTransport
 transport = UvxStdioTransport(
     from_package="git+https://github.com/zakahan/pypreader-mcp.git",
     tool_name="pypreader-mcp",
+    env_vars={
+        "python_path": sys.executable,
+        "logging_level": "ERROR",
+    },
 )
 
 
 async def main():
     async with Client(transport) as client:
+        print("> 1. List tools:")
         result = await client.list_tools()
         for tool in result:
             print(tool)
+        print("> 2. get_package:")
+        result = await client.call_tool(
+            name="get_package_directory", arguments={"package_name": "fastmcp"}
+        )
+        print(result)
 
 
 if __name__ == "__main__":
+    # uvx --from git+https://github.com/zakahan/pypreader-mcp.git pypreader-mcp --python_path xxxxx --logging_level ERROR
     asyncio.run(main())
